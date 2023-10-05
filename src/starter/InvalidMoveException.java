ea7d083cac06dc271a434670a1c669bd44adaa03
sam <samgerlach72@gmail.com>
2023-10-04 20:17:34 -0600

    Adding all starter code

---
+++ src/starter/InvalidMoveException.java
@@ -0,0 +1,11 @@
+/**
+ * Indicates an invalid move was made in a game
+ */
+public class InvalidMoveException extends Exception {
+
+    public InvalidMoveException() {}
+
+    public InvalidMoveException(String message) {
+        super(message);
+    }
+}
